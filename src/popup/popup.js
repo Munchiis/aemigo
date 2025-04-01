8b50df9b6fe858293cf65258c41f30ac22565bb7
Juan Echevarria <jechevarria00@gmail.com>
2025-04-01 15:32:01 -0700

    onclicks for shortcuts

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,12 +1,34 @@
-//load all categories from sites.json
-//harrison comment
+async function generateContent() {
+    try {
+        const { sitesMap } = await chrome.storage.local.get('sitesMap');
 
-//iterate through sitesMap, get each category, and then check session storage for that category (shortcut__categoryName)
+        for (const category of sitesMap.shortcuts) {
+            const { [`shortcut_${category.category}`]: html } =
+                await chrome.storage.local.get(`shortcut_${category.category}`);
 
-const sitesMap = await chrome.storage.local.get('sitesMap');
-const shortcut = sitesMap.sitesMap.shortcuts;
-shortcut.forEach(async (element) => {
-    const categoryHTML = `shortcut_${element.category}`
-    const html = await chrome.storage.local.get(categoryHTML);
-    document.body.innerHTML += html[categoryHTML];
-});
+            const fragment = document.createRange().createContextualFragment(html);
+            document.body.appendChild(fragment);
+
+        }
+
+        document.querySelectorAll('.path-link').forEach(link => {
+            link.addEventListener('click', async () => {
+                const href = link.getAttribute('data-href');
+
+                const pathUrl = new URL(href);
+                const pathname = pathUrl.pathname;
+
+                const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true });
+                const currentUrl = new URL(activeTab.url);
+
+                const newUrl = `${currentUrl.origin}${pathname}`;
+
+                await chrome.tabs.update(activeTab.id, { url: newUrl });
+            });
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', generateContent);

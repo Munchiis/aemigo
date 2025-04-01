2040be2090187f857da35734e0e11a80667767a5
Juan Echevarria <jechevarria00@gmail.com>
2025-04-01 11:13:53 -0700

    generate html children nodes

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,14 +1,13 @@
 //load all categories from sites.json
 //harrison comment
 
-//in event of clicking shortcut -> getcurrentab ->
-//create new url -> append shortcut pathname ->
-//make group -> add tab to that group
-async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-}
-const res = await getCurrentTab();
-console.log(res);
+//iterate through sitesMap, get each category, and then check session storage for that category (shortcut__categoryName)
+
+const sitesMap = await chrome.storage.local.get('sitesMap');
+const shortcut = sitesMap.sitesMap.shortcuts;
+shortcut.forEach(async (element) => {
+    const categoryHTML = `shortcut_${element.category}`
+    const html = await chrome.storage.local.get(categoryHTML);
+    document.body.innerHTML += html[categoryHTML];
+})
+

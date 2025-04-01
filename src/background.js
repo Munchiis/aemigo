79cfec0b098200010ed3b50fb847eb42c816bd0f
Juan Echevarria <jechevarria00@gmail.com>
2025-04-01 10:54:48 -0700

    working state of saving to local storage

--- src/background.js
+++ src/background.js
@@ -5,26 +5,46 @@
 //create all the potential elements based on this sites.json
 //
 
-async function initialInstall() {
+async function initShortcuts() {
     try {
         const response = await fetch(chrome.runtime.getURL('src/sites.json'));
         const data = await response.json();
         const shortcutsMap = {};
-        chrome.storage.local.set({ 'sitesData': data }, () => {
-            console.log('sitesData', data);
+
+        chrome.storage.local.set({ 'sitesMap': data }, () => {
             //stores within its own extension local storage
         });
-        data.shortcuts.forEach(element => {
-            //generate the html for each shortcut button
+        data.shortcuts.forEach(category => {
+            const categoryGroup = []
+            for (const path of category.paths) {
+                const pathObj = { ...path };
+                categoryGroup.push(pathObj);
+            }
+            shortcutsMap[category.category] = categoryGroup
         });
-        chrome.storage.local.set(shortcutsMap, () => {
-            //shortcuts html stored
-        });
+
+        //iterate through values of shortcuts map
+        for (const [shortcut, value] of Object.entries(shortcutsMap)) {
+            //shortcut is an object so just grab
+            console.log(shortcut)
+            console.log("value: ", value);
+            const html = `
+            <div class="title">
+            <ul>${value.map(path => `<li><a href="${path.url}">${path.title}</a></li>`).join('')}</ul>
+            </div>
+            `
+
+            chrome.storage.local.set({ [`shortcut_${shortcut}`]: html });
+        }
+
     } catch (error) {
         console.error(error);
     }
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-    initialInstall();
+    initShortcuts();
+})
+chrome.runtime.onStartup.addListener(() => {
+    initShortcuts();
 })

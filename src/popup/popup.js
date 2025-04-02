de9779ba5a3dc1ef9243117fb888607853ad8fc9
hrrsnsu <hrrsnsu@gmail.com>
2025-04-02 15:07:42 -0700

    styling for popup

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,15 +1,22 @@
 async function generateContent() {
     try {
         const { sitesMap } = await chrome.storage.local.get('sitesMap');
-
+        const shortcutContainer = document.querySelector('.Popup__shortcutContainer');
         for (const category of sitesMap.shortcuts) {
             const { [`shortcut_${category.category}`]: html } =
                 await chrome.storage.local.get(`shortcut_${category.category}`);
 
             const fragment = document.createRange().createContextualFragment(html);
-            document.body.appendChild(fragment);
+            shortcutContainer.appendChild(fragment);
 
         }
+
+        document.querySelectorAll('.Shortcut__category').forEach(category => {
+            category.addEventListener('click', () => {
+                const pathContainer = category.parentElement.querySelector('.Shortcut__pathContainer');
+                pathContainer.classList.toggle('display')
+            });
+        })
 
         document.querySelectorAll('.path-link').forEach(link => {
             link.addEventListener('click', async () => {

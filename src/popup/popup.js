832c7f13882cba77309309857d2f2afe01c1dd1a
Juan Echevarria <jechevarria00@gmail.com>
2025-04-03 10:25:55 -0700

    links open in new named groups

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -27,15 +27,56 @@ async function generateContent() {
 
                 const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true });
                 const currentUrl = new URL(activeTab.url);
+                const selectedEnvs = getSelectedEnv();
+                const server = sitesMap.servers.find((currentServer, index) => {
+                    const envList = currentServer.env.author.concat(currentServer.env.publisher, currentServer.env.dispatcher, currentServer.live)
+                    return envList.some((env) => new URL(env).origin === currentUrl.origin)
+                });
 
-                const newUrl = `${currentUrl.origin}${pathname}`;
-
-                await chrome.tabs.update(activeTab.id, { url: newUrl });
+                redirect(selectedEnvs, server.env, pathname, server.title)
             });
         });
     } catch (error) {
         console.error(error);
     }
+}
+
+function getSelectedEnv() {
+    const selectedEnvs = {}
+
+    const inputs = document.querySelectorAll('input[name="author"], input[name="dispatcher"], input[name="publisher"]')
+    inputs.forEach(input => {
+        selectedEnvs[input.name] = input.checked;
+    });
+    return selectedEnvs;
+}
+
+
+async function redirect(selectedEnvs, envs, pathname, name) {
+    const tabIds = []
+    if (selectedEnvs.author) {
+        for (const author of envs.author) {
+            const createdTabUrl = author + pathname;
+            const tab = await chrome.tabs.create({ active: false, url: createdTabUrl })
+            tabIds.push(tab.id);
+        }
+    }
+    if (selectedEnvs.dispatcher) {
+        for (const dispatcher of envs.dispatcher) {
+            const createdTabUrl = dispatcher + pathname;
+            const tab = await chrome.tabs.create({ active: false, url: createdTabUrl })
+            tabIds.push(tab.id)
+        }
+    }
+    if (selectedEnvs.publisher) {
+        for (const publisher of envs.publisher) {
+            const createdTabUrl = publisher + pathname;
+            const tab = await chrome.tabs.create({ active: false, url: createdTabUrl })
+            tabIds.push(tab.id)
+        }
+    }
+    const group = await chrome.tabs.group({ tabIds })
+    await chrome.tabGroups.update(group, { color: 'blue', title: name });
 }
 
 document.addEventListener('DOMContentLoaded', generateContent);
